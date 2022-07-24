f7e2527e30826ad861b1662070d4cdcc2ee7527a
Central <powerpawan@live.com>
2022-07-23 22:23:06 -0400

    Complete change of how grid is displayed, allow user to choose width and length

--- myscript.js
+++ myscript.js
@@ -1,21 +1,56 @@
+let setNum = 8;
+let trigger = false;
 
+const container = document.querySelector(".container");
 
-const container = document.querySelector('.container');
-let df = new DocumentFragment();
+function gridd() {
+  let df = new DocumentFragment();
+  for (let i = 0; i < setNum * setNum; i++) {
+    const makeGrid = document.createElement("div");
+    makeGrid.classList.add("makeGrid");
+    makeGrid.textContent = "";
+    df.appendChild(makeGrid);
+  }
+  container.appendChild(df);
 
-for(let i = 0; i < 256; i++) {   
-const makeGrid = document.createElement('div');
-makeGrid.classList.add('makeGrid');
-makeGrid.textContent = 'x';
-df.appendChild(makeGrid);
+  let weDraw = document.querySelectorAll(".makeGrid");
+  weDraw.forEach((makeGrid) => {
+    
+    makeGrid.addEventListener("mouseover", () => {
+        if(trigger === true){
+      makeGrid.style.backgroundColor = "red";
+   } });
+  });
 }
 
-container.appendChild(df);
-
-
-let weDraw = document.querySelectorAll('.makeGrid');
-weDraw.forEach((makeGrid)=>{ 
-    makeGrid.addEventListener('mouseover', () => {
-        makeGrid.style.backgroundColor ="red"});
+document.querySelector(".setNum").addEventListener("click", function () {
+  let numPrompt = prompt("Provide a number between 8-64");
+  if (numPrompt > 64) {
+    prompt("Not within the number range, try again!");
+  } else {
+    setNum = parseInt(numPrompt);
+    removeGrids();
+    let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+    parseInt(htmlStyles.getPropertyValue("--colNum"));
+    //container.style.gridTemplate= `repeat(${setNum}, 1fr)`;
+    document.documentElement.style.setProperty(`--colNum`,setNum)
+    gridd();
+  }
 });
 
+function removeGrids() {
+  let removeGrids = document.querySelectorAll(".makeGrid");
+  removeGrids.forEach((removeGrids) => {
+    removeGrids.remove();
+  });
+}
+
+document.addEventListener('mousedown', function() {
+    trigger = true;})
+
+    document.addEventListener('mouseup', function(){
+        trigger = false;
+    });
+    
+
+gridd();
